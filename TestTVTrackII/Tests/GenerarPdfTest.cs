5d7c5acd09276e26512a1e8bc24f07b37690ce12
caster009 <caster0999@gmail.com>
2025-04-23 00:16:18 -0600

    Ultimos Test

---
+++ TestTVTrackII/Tests/GenerarPdfTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTVTrackII.Tests
+{
+    [TestClass]
+    public class GenerarPdfTest
+    {
+        [TestMethod]
+        public void Pagina_GenerarPdf_Contiene_Boton_Descarga()
+        {
+            // Arrange:
+            // Simulamos el HTML que se renderiza en la página
+            string htmlSimulado = @"
+                <h2>Reporte PDF</h2>
+                <p>Haz clic en el botón para descargar el archivo PDF generado:</p>
+                <a asp-page-handler=""Descargar"" class=""btn btn-primary"">Descargar PDF</a>
+            ";
+
+            // Act:
+            // Verificamos si el HTML contiene el botón correcto para descargar el PDF
+            bool contieneBotonDescarga = htmlSimulado.Contains("asp-page-handler=\"Descargar\"") &&
+                                         htmlSimulado.Contains(">Descargar PDF<");
+
+            // Assert:
+            // Confirmamos que el botón esté presente
+            Assert.IsTrue(contieneBotonDescarga);
+        }
+    }
+}
